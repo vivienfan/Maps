2e8f7b49836173513db04f26ef14a91fbb69e34c
vagrant <vivien.fan.1224@gmail.com>
2017-05-24 21:36:10 +0000

    lists route

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,11 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dataHelper) => {
-
   router.post('/login', (req, res) => {
     let username = req.body.username;
     let email = req.body.email;
     let input_password = req.body.password;
+
     dataHelper.logIn(username, email, password, (err, uid, msg) => {
       if (err) {
         res.status(500).json({error: err.message});
@@ -30,11 +30,11 @@ module.exports = (dataHelper) => {
 
   router.post('/register', (req, res) => {
     let obj = {
-      name: req.body.name,
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     };
+
     dataHelper.createNewUser(obj, (err, uid, msg) => {
       if (err) {
         res.status(500).json({error: err.message});
--- routes/lists.js
+++ routes/lists.js
@@ -4,6 +4,17 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dataHelper) => {
+  router.get('/', (req, res) => {
+    let username = req.body.username;
+
+    dataHelper.getAllLists(username, (err, obj) => {
+      if (err) {
+        res.status(500).send();
+      } else {
+        res.status(200).json(obj);
+      }
+    });
+  });
 
   return router;
 }
