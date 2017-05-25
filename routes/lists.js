714e5eba4c35c30c4efdca9fd09e9f6fd7c5ca9a
vagrant <vivien.fan.1224@gmail.com>
2017-05-25 02:04:09 +0000

    lists - getAll, getOne, delete, addContributor, addNew

--- routes/lists.js
+++ routes/lists.js
@@ -8,7 +8,7 @@ module.exports = (dataHelper) => {
     let uid = req.session.user_id;
     dataHelper.getAllContributionForUser(uid, (err, contributions) => {
       if (err) {
-        res.status(500).send(err);
+        res.status(500).json({error: err.message});
       } else {
         let arr = [];
         let contris = [];
@@ -20,7 +20,7 @@ module.exports = (dataHelper) => {
         }
         dataHelper.getAllPublicListsExcept(arr, (err, publics) => {
           if (err) {
-            res.status(500).send(err);
+            res.status(500).send(json({error: err.message}));
           } else {
             res.status(200).json({contribution: contris, public: publics});
           }
@@ -29,16 +29,61 @@ module.exports = (dataHelper) => {
     });
   });
 
-  router.get('/:id', (req, res) => {
+  router.get('/:lid', (req, res) => {
     let uid = req.session.user_id;
-    dataHelper.getAllFavouritesForUser(uid, (err, arr) => {
-      if(err) {
-        res.status(500).send(err);
+    let lid = req.params.lid;
+    dataHelper.getListInfo(lid, (err, listInfo) => {
+      if (err) {
+        res.status(500).json({error: err.message});
       } else {
-        dataHelper.getContributionByListId(uid)
+        dataHelper.getContributorName(lid, (err, contributors) => {
+          if (err) {
+            res.status(500).json({error: err.message});
+          } else {
+            dataHelper.getMapsByListId(lid, (err, maps) => {
+              if (err) {
+                res.status(500).json({error: err.message});
+              } else {
+                res.status(200).json({
+                  listInfo: listInfo,
+                  contributors: contributors,
+                  maps: maps
+                })
+              }
+            });
+          }
+        });
       }
     });
   });
+
+  router.delete('/:lid', (req, res) => {
+    dataHelper.dropList(lid, (err) => {
+      if(err) {
+        res.status(500).json({error: err.message});
+      } else {
+        res.status(200).send();
+      }
+    })
+  });
+
+  router.add('/:lid/add_contributor', (req, res) => {
+    let obj = {
+      username: req.body.username,
+      lid: req.params.lid
+    }
+    dataHelper.addContributor(obj, (err) => {
+      if(err) {
+        res.status(500).send();
+      } else {
+        res.status(200).send();
+      }
+    });
+  });
+
+  router.post('/new', (req, res) => {
+
+  })
 
   return router;
 }
