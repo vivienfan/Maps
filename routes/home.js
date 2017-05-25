bc53b72dc90c160949aab82beb4f5f9f96dc61c1
vagrant <vivien.fan.1224@gmail.com>
2017-05-25 15:18:09 +0000

    comment on for all server side request handlers defined, implemented addList

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,24 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dataHelper) => {
+
+  // method: get
+  // URL: /
+  // client input: none
+  // server output: none
+  //
+  // render index.ejs
   router.get('/', (req, res) => {
     res.render('../views/index');
   });
 
+  // method: post
+  // URL: /login
+  // client input: req.body = { email: str, password: str }
+  // server output: err / { username: str } / msg
+  //
+  // check if email and password matches with db
+  // sets cookies if login success
   router.post('/login', (req, res) => {
     let obj = {
       email: req.body.email,
@@ -15,36 +29,50 @@ module.exports = (dataHelper) => {
     }
     dataHelper.logIn(obj, (err, dbObj, msg) => {
       if (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).send(err.message);
         return;
       }
       if (dbObj) {
         req.session.user_id = dbObj.uid;
         req.session.username = dbObj.username;
         req.session.email = dbObj.email;
-        console.log(req.session.user_id);
-        res.status(200).send();
+        console.log("-------------login: session.user_id", req.session.user_id);
+        res.status(200).json({username: dbObj.username});
       } else {
         res.status(403).send(msg);
       }
     });
   });
 
+  // method: post
+  // URL: /logout
+  // client input: none
+  // server output: none
+  //
+  // clear the cookies for the user
   router.post('/logout', (req, res) => {
     req.session.user_id = null;
+    req.session.username = null;
+    req.session.email = null;
     res.status(200).send();
   });
 
+  // method: post
+  // URL: /register
+  // client input: req.body = { username: str, email: str, password: str }
+  // server output: err/none/msg
+  //
+  // add a new user into the db
+  // sets cookies if register success
   router.post('/register', (req, res) => {
     let obj = {
-      // name: req.body.name,
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     };
     dataHelper.createNewUser(obj, (err, dbObj, msg) => {
       if (err) {
-        res.status(500).json({error: err.message});
+        res.status(403).send(err.message);
         return;
       }
       if (dbObj) {
@@ -52,8 +80,6 @@ module.exports = (dataHelper) => {
         req.session.username = dbObj.username;
         req.session.email = dbObj.email;
         res.status(200).send();
-      } else {
-        res.status(403).send(msg);
       }
     });
   });
--- routes/lists.js
+++ routes/lists.js
@@ -4,51 +4,70 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dataHelper) => {
+
+  // method: get
+  // URL: /lists
+  // client input: none
+  // server output: err / { contribution: [], public: [] }
+  // contribution, public = { l_id: int, title: str, description: str }
+  //
+  // main page, render all contributions
+  // and all the rest public lists ordered by number of favourites
   router.get('/', (req, res) => {
-    let uid = req.session.user_id;
-    dataHelper.getAllContributionForUser(uid, (err, contributions) => {
-      if (err) {
-        res.status(500).json({error: err.message});
-      } else {
-        let arr = [];
-        let contris = [];
-        if (contributions) {
-          contributions.forEach((element) => {
-            arr.push(element.l_id);
-            contris.push({title: element.title, descritpion:element.description});
-          });
-        }
-        dataHelper.getAllPublicListsExcept(arr, (err, publics) => {
-          if (err) {
-            res.status(500).send(json({error: err.message}));
-          } else {
-            res.status(200).json({contribution: contris, public: publics});
-          }
-        });
-      }
-    });
+    // let uid = req.session.user_id;
+    // dataHelper.getAllContributionForUser(uid, (err, contributions) => {
+    //   if (err) {
+    //     res.status(500).send(err.message);
+    //   } else {
+    //     let arr = [];
+    //     let contris = [];
+    //     if (contributions) {
+    //       contributions.forEach((element) => {
+    //         arr.push(element.l_id);
+    //         contris.push({title: element.title, descritpion:element.description});
+    //       });
+    //     }
+    //     dataHelper.getAllPublicListsExcept(arr, (err, publics) => {
+    //       if (err) {
+    //         res.status(500).send(err.message);
+    //       } else {
+    //         res.status(200).json({contribution: contris, public: publics});
+    //       }
+    //     });
+    //   }
+    // });
+    res.status(200);
   });
 
+
+  // method: get
+  // URL: /lists/:lid
+  // client input: req.params
+  // server output: err /
+  //  {
+  //    listInfo:     { title: str, description: str, public: bool },
+  //    contributors: [ { username: str } ],
+  //    maps:         [ { m_id: int, title: str, description: str } ]
+  //  }
+  //
+  // list page, get the list info(title, description, access)
+  // contributors and maps that belongs to the list
   router.get('/:lid', (req, res) => {
     let uid = req.session.user_id;
     let lid = req.params.lid;
     dataHelper.getListInfo(lid, (err, listInfo) => {
       if (err) {
-        res.status(500).json({error: err.message});
+        res.status(500).send(err.message);
       } else {
         dataHelper.getContributorName(lid, (err, contributors) => {
           if (err) {
-            res.status(500).json({error: err.message});
+            res.status(500).send(err.message);
           } else {
             dataHelper.getMapsByListId(lid, (err, maps) => {
               if (err) {
-                res.status(500).json({error: err.message});
+                res.status(500).send(err.message);
               } else {
-                res.status(200).json({
-                  listInfo: listInfo,
-                  contributors: contributors,
-                  maps: maps
-                })
+                res.status(200).json({ listInfo: listInfo, contributors: contributors, maps: maps });
               }
             });
           }
@@ -57,32 +76,62 @@ module.exports = (dataHelper) => {
     });
   });
 
+  // method: delete
+  // URL: /lists/:lid
+  // client input: req.param
+  // server output: err / none
+  //
+  // deletes the list from database
+  // TODO: redirect?
   router.delete('/:lid', (req, res) => {
     dataHelper.dropList(lid, (err) => {
       if(err) {
-        res.status(500).json({error: err.message});
+        res.status(500).send(err.message);
       } else {
         res.status(200).send();
       }
     })
   });
 
-  router.post('/:lid/add_contributor', (req, res) => {
+  // method: post
+  // URL: /lists/:id/addContributor
+  // client input: req.param, req.body = { username: str }
+  // server output: none
+  //
+  // add a new contributor into the contributions table
+  router.post('/:lid/addContributor', (req, res) => {
     let obj = {
       username: req.body.username,
       lid: req.params.lid
     }
     dataHelper.addContributor(obj, (err) => {
       if(err) {
-        res.status(500).send();
+        res.status(500).send(err.message);
       } else {
         res.status(200).send();
       }
     });
   });
 
+  // method: post
+  // URL: /lists/new
+  // client iput: req.body = { title: str, description: str, public: bool }
+  // server output: { lid: int }
+  //
+  // add a new list into the lists table
   router.post('/new', (req, res) => {
-
+    let obj = {
+      title: req.body.title,
+      description: req.body.description,
+      public: req.body.public
+    };
+    dataHelper.addList(obj, (err, lid) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.status(200).json({ lid: lid });
+      }
+    });
   })
 
   return router;
