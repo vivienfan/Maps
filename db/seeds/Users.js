51147eeb6937573b676515dbe0bd803520f25246
vagrant <vivien.fan.1224@gmail.com>
2017-05-25 21:01:57 +0000

    seeds

---
+++ db/seeds/Users.js
@@ -0,0 +1,71 @@
+let users = [
+  {id: 1,   name: 'Alice',    email:'alice@h.com',    password:'alice',     username:'alice'},
+  {id: 2,   name: 'Bob',      email:'bob@h.com',      password:'bob',       username:'bob'},
+  {id: 3,   name: 'Charlie',  email:'charlie@h.com',  password:'charlie',   username:'charlie'}
+];
+
+let lists = [
+  {l_id: 1,   title: 'apple',   description: 'description',   public: true}
+];
+
+let maps = [
+  {m_id: 1,   l_id: 1,  title: 'map 1',   description: 'something'},
+  {m_id: 2,   l_id: 1,  title: 'map 2',   description: 'something'},
+  {m_id: 3,   l_id: 1,  title: 'map 3',   description: 'something'}
+];
+
+let map_points = [
+  {p_id: 1,   m_id: 1,  title: 'fruits',  description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3 },
+  {p_id: 2,   m_id: 2,  title: 'the',     description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3},
+  {p_id: 3,   m_id: 3,  title: '?',       description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3}
+];
+
+let contributions = [
+  {u_id: 3,   l_id: 1},
+  {u_id: 2,   l_id: 1}
+];
+
+let favourites = [
+  {u_id: 1,   l_id: 1}
+];
+
+exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('users').del(),
+    knex('lists').del(),
+    knex('maps').del(),
+    knex('map_points').del(),
+    knex('contributions').del(),
+    knex('favourites').del()
+  ])
+  .then(() => {
+    return knex('users').insert(users);
+  })
+  .then(() => {
+    return knex('lists').insert(lists);
+  })
+  .then(() => {
+    return knex('maps').insert(maps);
+  })
+  .then(() => {
+    return knex('map_points').insert(map_points);
+  })
+  .then(() => {
+    return knex('contributions').insert(contributions);
+  })
+  .then(() => {
+    return knex('favourites').insert(favourites);
+  });
+};
+
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('users').del()
+//     .then(function () {
+//       return knex('users').insert([
+//         {id: 1,   name: 'Alice',    email:'alice@h.com',    password:'alice',     username:'alice'},
+//         {id: 2,   name: 'Bob',      email:'bob@h.com',      password:'bob',       username:'bob'}
+//       ]);
+//     });
+// };
--- db/seeds/data.js
+++ db/seeds/data.js
@@ -1,82 +1,49 @@
-exports.seed = function(knex, Promise) {
-  return
-  knex('users').del().insert([
-      {id: 1, name: 'Alice', email:'alice@h.com', password:'alice', username:'alice'},
-      {id: 2, name: 'Bob', email:'bob@h.com', password:'bob', username:'bob'},
-      {id: 3, name: 'Charlie', email:'charlie@h.com', password:'charlie', username:'charlie'}
-    ])
-  .then(function() {
-    knex('lists').del().insert({l_id: 1, title: 'apple', description: 'description', public: true});
-  })
-  .then(function() {
-    knex('maps').del().insert([
-      {m_id: 1, l_id: 1},
-      {m_id: 2, l_id: 1},
-      {m_id: 3, l_id: 1}
-    ]);
-  })
-  .then(function() {
-    knex('map_points').del().insert([
-      {p_id: 1, m_id: 1, title: 'fruits', description: 'something', image:'image', longitude: -33.3, latitude:33.3 },
-      {p_id: 2, m_id: 2, title: 'the', description: 'something', image:'image', longitude: -33.3, latitude: 33.3},
-      {p_id: 3, m_id: 3, title: '?', description: 'something', image:'image', longitude: -33.3, latitude: 33.3}
-    ]);
-  })
-  .then(function() {
-    knex('contributions').del().insert([
-      {u_id: 1, l_id: 1},
-      {u_id: 2, l_id: 1}
-    ]);
-  })
-  .then(function() {
-    knex('favourites').del().insert([
-      {u_id: 3, l_id: 1}
-    ]);
-  });
+exports.seed = function(knex, Promise) {}
+//   return
+//     //Promise.all([knex('users').del().then( () => {
 
-  // Promise.all([
-  //   knex('users').del().then( function() {
-  //     return knex('users').insert([
-  //       {id: 1, name: 'Alice', email:'alice@h.com', password:'alice', username:'alice'},
-  //       {id: 2, name: 'Bob', email:'bob@h.com', password:'bob', username:'bob'},
-  //       {id: 3, name: 'Charlie', email:'charlie@h.com', password:'charlie', username:'charlie'}
-  //     ]);
-  //   }),
-  //   knex('lists').del().then( function() {
-  //     return knex('lists').insert([
-  //       {l_id: 1, title: 'apple', description: 'description', public: true}
-  //     ]);
-  //   }),
-  //  knex('maps').del().then( function() {
-  //     return knex('maps').insert([
-  //       {m_id: 1, l_id: 1},
-  //       {m_id: 2, l_id: 1},
-  //       {m_id: 3, l_id: 1}
-  //     ]);
-  //   }),
-   // knex('map_points').del().insert([
-   //      {p_id: 1, m_id: 1, title: 'fruits', description: 'something', image:'image', longitude: -33.3, latitude:33.3 },
-   //      {p_id: 2, m_id: 2, title: 'the', description: 'something', image:'image', longitude: -33.3, latitude: 33.3},
-   //      {p_id: 3, m_id: 3, title: '?', description: 'something', image:'image', longitude: -33.3, latitude: 33.3}
-   //    ]);
-   //  })
-      // knex('contributions').del().then( function() {
-      //   return knex('contributions').insert([
-      //   {p_id: 1, m_id: 1, title: 'fruits', description: 'something', image:'image', longitude: -33.3, latitude:33.3 },
-      //   {p_id: 2, m_id: 2, title: 'the', description: 'something', image:'image', longitude: -33.3, latitude: 33.3},
-      //   {p_id: 3, m_id: 3, title: '?', description: 'something', image:'image', longitude: -33.3, latitude: 33.3}
-      //     ]);
-      // }),
-
-      // knex('favourites').del().then( function() {
-      //   return knex('favourites').insert([
-      //   {p_id: 1, m_id: 1, title: 'fruits', description: 'something', image:'image', longitude: -33.3, latitude:33.3 },
-      //   {p_id: 2, m_id: 2, title: 'the', description: 'something', image:'image', longitude: -33.3, latitude: 33.3},
-      //   {p_id: 3, m_id: 3, title: '?', description: 'something', image:'image', longitude: -33.3, latitude: 33.3}
-      //     ]);
-      // }),
-
-
-
-  // ]);
-};
+//   //    return knex('users').insert([
+//   Promise.all([knex('users').insert([
+//         {id: 1,   name: 'Alice',    email:'alice@h.com',    password:'alice',     username:'alice'},
+//         {id: 2,   name: 'Bob',      email:'bob@h.com',      password:'bob',       username:'bob'},
+//         {id: 3,   name: 'Charlie',  email:'charlie@h.com',  password:'charlie',   username:'charlie'}
+//     ])])
+//   //})
+// //])
+//     //   return knex('users').insert([
+//     //     {id: 1,   name: 'Alice',    email:'alice@h.com',    password:'alice',     username:'alice'},
+//     //     {id: 2,   name: 'Bob',      email:'bob@h.com',      password:'bob',       username:'bob'},
+//     //     {id: 3,   name: 'Charlie',  email:'charlie@h.com',  password:'charlie',   username:'charlie'}
+//     //   ])
+//     // }).then( () => { console.log('done')})
+//     .then(() => {
+//       return knex('lists').del().insert([
+//         {l_id: 1,   title: 'apple',   description: 'description',   public: true}
+//       ]);
+//     })
+//     .then(() => {
+//       knex('maps').del().insert([
+//         {m_id: 1,   l_id: 1,  title: 'map 1',   description: 'something'},
+//         {m_id: 2,   l_id: 1,  title: 'map 2',   description: 'something'},
+//         {m_id: 3,   l_id: 1,  title: 'map 3',   description: 'something'}
+//       ]);
+//     })
+//     .then(() => {
+//       knex('map_points').del().insert([
+//         {p_id: 1,   m_id: 1,  title: 'fruits',  description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3 },
+//         {p_id: 2,   m_id: 2,  title: 'the',     description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3},
+//         {p_id: 3,   m_id: 3,  title: '?',       description: 'something',   image:'https://www.w3schools.com/css/trolltunga.jpg',  longitude: -33.3,   latitude: 33.3}
+//       ]);
+//     })
+//     .then(() => {
+//       knex('contributions').del().insert([
+//         {u_id: 1,   l_id: 1},
+//         {u_id: 2,   l_id: 1}
+//       ]);
+//     })
+//     .then(() => {
+//       knex('favourites').del().insert([
+//         {u_id: 3,   l_id: 1}
+//       ]);
+//     });
+// };
