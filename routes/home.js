5c0332c0a44eff2ff9cd4293d8bc8482033859be
vagrant <vivien.fan.1224@gmail.com>
2017-05-25 00:07:31 +0000

    removed random string generated by merge

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,6 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (dataHelper) => {
-<<<<<<< HEAD
   router.get('/', (req, res) => {
     res.render('../views/index');
   });
@@ -15,14 +14,6 @@ module.exports = (dataHelper) => {
       password: req.body.password
     }
     dataHelper.logIn(obj, (err, dbObj, msg) => {
-=======
-  router.post('/login', (req, res) => {
-    let username = req.body.username;
-    let email = req.body.email;
-    let input_password = req.body.password;
-
-    dataHelper.logIn(username, email, password, (err, uid, msg) => {
->>>>>>> 2e8f7b49836173513db04f26ef14a91fbb69e34c
       if (err) {
         res.status(500).json({error: err.message});
         return;
@@ -46,20 +37,12 @@ module.exports = (dataHelper) => {
 
   router.post('/register', (req, res) => {
     let obj = {
-<<<<<<< HEAD
       // name: req.body.name,
-=======
->>>>>>> 2e8f7b49836173513db04f26ef14a91fbb69e34c
       username: req.body.username,
       email: req.body.email,
       password: req.body.password
     };
-<<<<<<< HEAD
     dataHelper.createNewUser(obj, (err, dbObj, msg) => {
-=======
-
-    dataHelper.createNewUser(obj, (err, uid, msg) => {
->>>>>>> 2e8f7b49836173513db04f26ef14a91fbb69e34c
       if (err) {
         res.status(500).json({error: err.message});
         return;
