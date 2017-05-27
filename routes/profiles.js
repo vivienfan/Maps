c37caa2cfa882ffb17e690193940621996f477ee
vagrant <vivien.fan.1224@gmail.com>
2017-05-27 01:38:43 +0000

    tempvar

--- routes/profiles.js
+++ routes/profiles.js
@@ -34,6 +34,8 @@ module.exports = (dataHelper) => {
       })
     })
     .then(() => {
+      console.log(favourite);
+      console.log(contribution);
       console.log('render page');
       let tempVar = {
         username: username,
